3ec5d95b68fdb7af369ef20a7da207cd660d3d4f
test <test@example.com>
2019-06-16 15:00:46 +0800

    done

--- fixture/any-api.mock.ts
+++ fixture/any-api.mock.ts
@@ -1,28 +1,29 @@
+interface IGetList0Resp {
+    foo: 'bar'
+}
 export class AnyApi {
-    getList(id: number): Promise<{ foo: "bar"; }> {
+    constructor(public http: any) {
+        
+    }
+    getList(id: number): Promise<{ foo: 'bar' }> {
         return Promise.resolve({
             foo: "bar",
         })
     }
-
     getList0(id: number): Promise<IGetList0Resp> {
         return Promise.resolve({
             foo: "bar",
         })
     }
-
     getList1(id: number): Promise<string> {
         return Promise.resolve('any-ramdom-string')
     }
-
     getList2(id: number): Promise<number> {
         return Promise.resolve(100020)
     }
-
     getList3(id: number): Promise<void> {
         return Promise.resolve()
     }
-
     getList4(id: number): Promise<boolean> {
         return Promise.resolve(true)
     }
--- package/program.ts
+++ package/program.ts
@@ -1,5 +1,6 @@
 import { Project, ts, Type, CodeBlockWriter } from 'ts-morph'
 import * as path from 'path'
+import * as fs from 'fs'
 
 export class Program {
     constructor() {
@@ -7,9 +8,22 @@ export class Program {
     }
     parseApiFile(pathStr: string) {
         const prj = new Project()
-        const anyApiSrc = prj.addExistingSourceFile(pathStr)
+        const fileName = path.basename(pathStr, '.ts')
+        const mockFileName = `${fileName}.mock.ts`
+        const mockFilePath = path.dirname(pathStr) + '/' + mockFileName
+        if (fs.existsSync(path.resolve(mockFilePath))) {
+            throw new Error('file existed') 
+        }
+        fs.copyFileSync(pathStr, mockFilePath)
+        const anyApiSrc = prj.addExistingSourceFile(mockFilePath)
         const classList = anyApiSrc.getClasses()
+        for (const classItem of classList) {
+            console.log('class:', classItem.getName())
+        }
         if (!classList.length) {
+            throw new Error('not found any class') 
+        }
+        if (classList.length > 1) {
             throw new Error('only support one class per api file') 
         }
         const onlyClass = classList[0]
@@ -18,50 +32,36 @@ export class Program {
         }
         const methodList = onlyClass.getMethods()
         if (!methodList.length) {
-            throw new Error('not found any method inside the class') 
+            throw new Error('not found any method inside the class')
         }
-        const fileName = path.basename(pathStr, '.ts')
-        const mockFileName = `${fileName}.mock.ts`
-        const mockApiSrc = prj.createSourceFile(path.dirname(pathStr) + '/' + mockFileName)
-        mockApiSrc.addClass({
-            isExported: true,
-            name: onlyClass.getName(),
-            methods: methodList.map(method => {
-                console.log('mocking api:', method.getName())
-                return {
-                    name: method.getName(),
-                    parameters: method.getParameters().map(para => para.getStructure()),
-                    returnType: (writer) => {
-                        writer.write(method.getReturnType().getText())
-                    },
-                    statements: (writer) => {
-                        const returnTypeTxt = method.getReturnTypeNodeOrThrow().getText()
-                        if (!returnTypeTxt.match(/^Promise<[^>]+>/)) {
-                            throw new Error('return type must be Promise') 
-                        }
-                        // console.log('return type:', returnTypeTxt)
-                        const resolvedReturnType = method.getReturnType().getTypeArguments()[0]
-                        console.log('resolved return type:', resolvedReturnType.getText())
-                        writer.write(`return Promise.resolve(${this.resolveMock(resolvedReturnType)})`)
-                    }
+        const methods = onlyClass.getMethods()
+        for (const method of methods) {
+            method.setBodyText(writer => {
+                const returnTypeTxt = method.getReturnTypeNodeOrThrow().getText()
+                if (!returnTypeTxt.match(/^Promise<[^>]+>/)) {
+                    throw new Error('return type must be Promise')
                 }
+                // console.log('return type:', returnTypeTxt)
+                const resolvedReturnType = method.getReturnType().getTypeArguments()[0]
+                console.log('resolved return type:', resolvedReturnType.getText())
+                writer.write(`return Promise.resolve(${this.resolveMock(resolvedReturnType)})`)
             })
-        })
+        }
         prj.save()
     }
     private resolveMock(returnType: Type<ts.Type>) {
         if (returnType.isString()) {
-            return "'any-ramdom-string'" 
+            return "'any-ramdom-string'"
         } else if (returnType.isStringLiteral()) {
-            return returnType.getText() 
+            return returnType.getText()
         } if (returnType.isNumber()) {
-            return 100020 
+            return 100020
         } else if (returnType.isNullable()) {
             return 'void'
         } else if (returnType.getText() === 'void') {
             return ''
         } else if (returnType.isBoolean()) {
-            return true 
+            return true
         } else if (returnType.isObject() || returnType.isInterface()) {
             debugger
             const cdWriter = this.getCdWriter();
